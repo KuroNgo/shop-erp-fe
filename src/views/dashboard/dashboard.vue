a4a3d8ab126b3cef42569bf9e870273bcd6be846
KuroNgo <hoaiphong01012002@gmail.com>
2024-09-30 11:02:38 +0700

    upd: dashboard

--- src/views/dashboard/dashboard.vue
+++ src/views/dashboard/dashboard.vue
@@ -1,158 +1,171 @@
 <template>
-    <!-- Navbar -->
-    <div class=" flex justify-between bg-teal-700 h-[70px] w-full color absolute z-10">
-        <div class="flex items-center translate-x-8 ">
-            <img src="" alt="">
-            <p class="text-xl font-bold">LOGO</p>
-        </div>
-        <div class="flex items-center nav-custom">
-            <div class="grid text-right custom-transform-y nav-content p-2">
-                <label for="avatar" class=" font-bold">Ngo Hoai Phong</label>
-                <label for="avatar" class="translate-y-2">Admin</label>
-            </div>
-        </div>
-    </div>
-    <div class="w-full flex translate-y-20 h-full absolute">
-        <div class="content block w-[290px] bg-teal-100 overflow-y-auto overflow-x-hidden pd-10 ">
-            <div class="relative">
-                <div class="translate-x-2 translate-y-2">
-                </div>
-                <Divider :fontSize=fontSize value="OVERVIEW" class="translate-y-6 font-bold divider" />
-                <div class="translate-x-2 translate-y-10">
-
-                </div>
-
-                <div class="translate-x-2 translate-y-16">
-
-                </div>
-            </div>
-        </div>
-        <div class="w-[100%] h-full overflow-y-auto overflow-x-hidden bg-slate-100">
-            <div class="custom-transform-2 flex w-full justify-between ">
-                <div class="grid">
-                    <p class="text-5xl font-bold">Dashboard</p>
-                    <p class="text-sm translate-x-1">You last login: 21h ago NewZealand</p>
-                </div>
-            </div>
-            <div class="custom-transform-3 ">
-                <div class="flex">
-                    <div class="w-[200px] border-custom flex justify-between bg-white">
-                        <div class="flex justify-end">
-                            <div class="relative">
-                                <icon value="home"
-                                    styles="border:1px solid #000;background-color:white;border-radius:15px;padding:2px;color:blue"
-                                    classes="" class="icon custom-transform-4" />
-                            </div>
-                            <div class="translate-x-10">
-                                <p class="text-2xl font-extralight">Total Sales</p>
-                                <p class="text-lg font-sm">1106 > 6202 Sale</p>
-                            </div>
-                        </div>
-                        <div class="grid">
-                            <!-- <p class="text-2xl font-bold">26.13M$</p>
-                            <p class="text-lg font-thin">Total Sales</p> -->
-                        </div>
-                    </div>
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
-        </div>
+    <div class="w-full h-full bg-slate-200">
+        <el-container class="h-full w-full">
+            <el-aside width="300px" class="bg-white shadow-lg ">
+                <el-row :span="12">
+                    <el-divider content-position="left">OVERVIEW</el-divider>
+                    <el-menu default-active="1" class="ml-3 w-full" @open="handleOpen" @close="handleClose">
+                        <el-menu-item index="1" @click="activeView = 'dashboard'">
+                            <el-icon>
+                                <OfficeBuilding />
+                            </el-icon>
+                            <span>Dashboard</span>
+                        </el-menu-item>
+                        <el-menu-item index="2" @click="activeView = 'user-settings'">
+                            <el-icon>
+                                <User />
+                            </el-icon>
+                            <span>User settings</span>
+                        </el-menu-item>
+                        <el-divider content-position="left" class="-translate-x-4">DEPARTMENTS</el-divider>
+                        <el-sub-menu index="3" class="">
+                            <template #title>
+                                <el-icon>
+                                    <Notebook />
+                                </el-icon>
+                                <span>Account</span>
+                            </template>
+                            <el-menu-item-group>
+                                <template #title><span>Employee</span></template>
+                                <el-menu-item index="1-1">item one</el-menu-item>
+                                <el-menu-item index="1-2">item two</el-menu-item>
+                            </el-menu-item-group>
+                            <el-menu-item-group title="Group Two">
+                                <el-menu-item index="1-3">Candidater</el-menu-item>
+                            </el-menu-item-group>
+                            <el-sub-menu index="1-4">
+                                <template #title>Candidater</template>
+                                <el-menu-item index="1-4-1">item one</el-menu-item>
+                            </el-sub-menu>
+                        </el-sub-menu>
+                        <el-sub-menu index="4" class="">
+                            <template #title>
+                                <el-icon>
+                                    <Connection />
+                                </el-icon>
+                                <span>Human Resource</span>
+                            </template>
+                            <el-menu-item-group>
+                                <template #title><span>Employee</span></template>
+                                <el-menu-item index="1-1">item one</el-menu-item>
+                                <el-menu-item index="1-2">item two</el-menu-item>
+                            </el-menu-item-group>
+                            <el-menu-item-group title="Group Two">
+                                <el-menu-item index="1-3">Candidater</el-menu-item>
+                            </el-menu-item-group>
+                            <el-sub-menu index="1-4">
+                                <template #title>Candidater</template>
+                                <el-menu-item index="1-4-1">item one</el-menu-item>
+                            </el-sub-menu>
+                        </el-sub-menu>
+                        <el-sub-menu index="5" class="">
+                            <template #title>
+                                <el-icon>
+                                    <Service />
+                                </el-icon>
+                                <span>Sale and Distribution</span>
+                            </template>
+                            <el-menu-item-group>
+                                <template #title><span>Employee</span></template>
+                                <el-menu-item index="1-1">item one</el-menu-item>
+                                <el-menu-item index="1-2">item two</el-menu-item>
+                            </el-menu-item-group>
+                            <el-menu-item-group title="Group Two">
+                                <el-menu-item index="1-3">Candidater</el-menu-item>
+                            </el-menu-item-group>
+                            <el-sub-menu index="1-4">
+                                <template #title>Candidater</template>
+                                <el-menu-item index="1-4-1">item one</el-menu-item>
+                            </el-sub-menu>
+                        </el-sub-menu>
+                        <el-sub-menu index="6" class="">
+                            <template #title>
+                                <el-icon>
+                                    <House />
+                                </el-icon>
+                                <span>Warehouse</span>
+                            </template>
+                            <el-menu-item-group>
+                                <template #title><span>Employee</span></template>
+                                <el-menu-item index="1-1">item one</el-menu-item>
+                                <el-menu-item index="1-2">item two</el-menu-item>
+                            </el-menu-item-group>
+                            <el-menu-item-group title="Group Two">
+                                <el-menu-item index="1-3">Candidater</el-menu-item>
+                            </el-menu-item-group>
+                            <el-sub-menu index="1-4">
+                                <template #title>Candidater</template>
+                                <el-menu-item index="1-4-1">item one</el-menu-item>
+                            </el-sub-menu>
+                        </el-sub-menu>
+                        <el-divider content-position="left" class="-translate-x-4">APPROVAL</el-divider>
+                        <el-menu-item index="7" @click="activeView = 'chat'">
+                            <el-icon>
+                                <Message />
+                            </el-icon>
+                            <span>Chat</span>
+                        </el-menu-item>
+                        <el-menu-item index="8" @click="activeView = 'todo'">
+                            <el-icon><Select /></el-icon>
+                            <span>To Do</span>
+                        </el-menu-item>
+                        <el-divider content-position="left" class="-translate-x-4">SETTINGS</el-divider>
+                        <el-menu-item index="9" @click="activeView = 'settings'">
+                            <el-icon>
+                                <Setting />
+                            </el-icon>
+                            <span>Settings</span>
+                        </el-menu-item>
+                    </el-menu>
+                </el-row>
+            </el-aside>
+            <el-container>
+                <el-header class="bg-white m-4 rounded-md shadow-lg">
+                    <el-menu ellipsis-icon mode="horizontal" :popper-offset="16" style="max-width: 600px">
+                        <el-menu-item index="1">
+                            <el-icon><Select /></el-icon>
+                            <span>Processing Center</span>
+                        </el-menu-item>
+                        <el-sub-menu index="2">
+                            <template #title>Workspace</template>
+                            <el-menu-item index="2-1">item one</el-menu-item>
+                            <el-menu-item index="2-2">item two</el-menu-item>
+                            <el-menu-item index="2-3">item three</el-menu-item>
+                            <el-sub-menu index="2-4">
+                                <template #title>item four</template>
+                                <el-menu-item index="2-4-1">item one</el-menu-item>
+                                <el-menu-item index="2-4-2">item two</el-menu-item>
+                                <el-menu-item index="2-4-3">item three</el-menu-item>
+                            </el-sub-menu>
+                        </el-sub-menu>
+                    </el-menu>
+                </el-header>
+                <el-main class="h-full">
+                    <dashboard v-if="activeView === 'dashboard'" />
+                    <chat v-if="activeView === 'chat'" />
+                    <todo v-if="activeView === 'todo'" />
+                    <settings v-if="activeView === 'settings'" />
+                    <usersettings v-if="activeView === 'user-settings'" />
+                </el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
-<script setup lang="ts">
-import { onBeforeUnmount, onMounted, ref } from 'vue';
+<script lang="ts" setup>
+import { ref } from 'vue'
+import chat from './sidebar/chatview.vue';
+import dashboard from './sidebar/dashboardview.vue'
+import todo from './sidebar/todo.vue';
+import settings from './sidebar/settings.vue';
+import usersettings from './sidebar/usersettings.vue';
 
-const activeElement = ref('Dashboard');
-const minimized = ref<boolean>(false);
-const activeItem = ref<string | null>(null);
-const fontSize = ref('12px');
+const activeView = ref('dashboard')
 
-const checkScreenSize = () => {
-    updateFontSize();
-    minimized.value = window.innerWidth < 500;
-};
-
-const updateFontSize = () => {
-    if (window.innerWidth < 500) {
-        fontSize.value = '0px'
-    } else {
-        fontSize.value = '12px'
-    }
+const handleOpen = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
 }
-
-onMounted(() => {
-    checkScreenSize();
-    window.addEventListener('resize', checkScreenSize);
-})
-
-onBeforeUnmount(() => {
-    window.removeEventListener('resize', checkScreenSize)
-})
-
+const handleClose = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+}
 </script>
-
-<style scoped lang="css">
-.overflow {
-    overflow-y: hidden;
-}
-
-.custom-transform-y {
-    transform: translateY(-3px);
-}
-
-.nav-custom {
-    transform: translate(-20px, 0px);
-}
-
-.custom-transform {
-    transform: translate(5px, 15px);
-}
-
-.custom-transform-2 {
-    transform: translate(2rem, 1rem);
-}
-
-.custom-transform-3 {
-    transform: translate(2rem, 3rem);
-}
-
-.custom-transform-4 {
-    transform: translate(6px, -13px);
-}
-
-.color {
-    color: white;
-}
-
-aside {
-    background-color: rgba(255, 255, 255, 0) !important;
-}
-
-.icon {
-    position: absolute;
-    font-size: 20px;
-    top: 50%;
-    left: 50%;
-}
-
-.border-b-custom {
-    border-bottom: 1px solid #505050;
-}
-
-.border-custom {
-    border: 1px solid #000;
-}
-
-@media screen and (max-width: 500px) {
-    .content {
-        width: 100px;
-        min-width: 85px;
-    }
-
-    .nav-content {
-        font-size: 0px;
-    }
-}
-</style>
---
+++ src/views/dashboard/sidebar/dashboardview.vue
@@ -0,0 +1,2 @@
+<template>Đây là dashboard router</template>
+<script setup lang="ts"></script>
